74ae6a5979374ab5b3c646e60f0757104c7d5eed
Arvind Kalyankumar <arvindkalyankumar@gmail.com>
2017-11-28 20:53:04 +0100

    Initial app

---
+++ src/test/java/com/sebis/cepengineservice/CepEngineServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sebis.cepengineservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CepEngineServiceApplication.class)
+public class CepEngineServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
